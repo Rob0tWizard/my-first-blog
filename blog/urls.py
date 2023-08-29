8a52159c47e16fa0b51ec661eb36488181d134f0
Rob0tWizard <ilia.dementev65@gmail.com>
2023-08-29 14:29:31 +0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+#post/ значит, что после начала строки URL должен содержать слово post и косую черту /.
+#<int:pk> — означает, что Django ожидает целочисленное значение и преобразует его в представление — переменную pk.
+#  / — затем нам нужен еще один символ / перед тем, как адрес закончится.
 
 ]
+
