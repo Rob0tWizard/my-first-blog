572593a1946afcfeb95cf9be5a86d59eb09d0c17
Rob0tWizard <ilia.dementev65@gmail.com>
2023-08-30 17:19:20 +0700

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-#post/ значит, что после начала строки URL должен содержать слово post и косую черту /.
-#<int:pk> — означает, что Django ожидает целочисленное значение и преобразует его в представление — переменную pk.
-#  / — затем нам нужен еще один символ / перед тем, как адрес закончится.
+    # post/ значит, что после начала строки URL должен содержать слово post и косую черту /.
+    # <int:pk> — означает, что Django ожидает целочисленное значение и преобразует его в представление — переменную pk.
+    #  / — затем нам нужен еще один символ / перед тем, как адрес закончится.
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 
 ]
-
