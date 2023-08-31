ce978d7fa7a5ba1af567dc6a80a03e9d40ee3479
Rob0tWizard <ilia.dementev65@gmail.com>
2023-08-31 15:08:44 +0700

    drafts, publish list, remove, edit

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def post_new(request):
         if form.is_valid() :                #Фактически мы выполняем две операции: сохраняем форму form.save и добавляем автора
             post = form.save(commit=False)  #означает, что мы пока не хотим сохранять модель Post — сначала нужно добавить автора.
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)      #Теперь мы можем сделать переадресацию на страницу post_detail для созданной записи
     else:
@@ -36,10 +36,28 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #   post.published_date = timezone.now() если убираем эту строку то можно создавать draft посты
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+
+def post_publish(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect("post_detail", pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
 
